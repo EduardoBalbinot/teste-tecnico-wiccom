de0ddc8209ac5f7937742d7f0da96a5dc7513515
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-10-30 18:30:09 -0300

    chore: Header and HeaderCarousel component files created

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import HeaderCarousel from "./HeaderCarousel"
+
+export default function Header() {
+  return (
+    <header>
+      <HeaderCarousel />
+      <h1>Site Header</h1>
+    </header>
+  );
+}
---
+++ components/HeaderCarousel.tsx
@@ -0,0 +1,7 @@
+export default function HeaderCarousel() {
+  return (
+    <div>
+      <h2>Header Carousel Component</h2>
+    </div>
+  );
+}
