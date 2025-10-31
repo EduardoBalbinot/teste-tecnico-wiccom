aef025ea1a48a126bae814a8ab00be337ab24ec7
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-10-31 03:30:37 -0300

    feat: Hero done

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -15,11 +15,13 @@ export default function HeaderNav({ menus = [] }: Props) {
   return (
     <nav className="border-t border-b border-[#F1F1F1] px-10 font-semibold text-sm text-[#404042] relative">
       <ul className="flex justify-center gap-8 tracking-[-0.28px]">
-        {menus.map((menu, index) => (<li key={index} className="flex px-2 group pb-3 pt-3.5 relative">
+        {menus.map((menu, index) => (<li key={index} className="flex px-2 group pb-3 pt-3.5 relative hover:text-[#056939]">
           <a className="px-4 flex gap-2 cursor-pointer" href={menu.href || "/"}>
             {menu.label}
             {menu.submenus && <img src="/images/UI/arrowdown.svg" alt="Seta para baixo" />}
-            {menu.submenus && <div
+            
+          </a>
+          {menu.submenus && <div
             className="min-h-28 w-full z-10 absolute top-full left-0 hidden group-hover:block opacity-0 group-hover:opacity-100">
               <div className="bg-white shadow-lg flex flex-col p-4 gap-3">
                 {menu.submenus.map((submenu, subIndex) => (
@@ -33,8 +35,6 @@ export default function HeaderNav({ menus = [] }: Props) {
                 ))}
               </div>
           </div>}
-          </a>
-          
         </li>))}
       </ul>
     </nav>
---
+++ sections/Hero.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section className="pt-[50px] pb-[43px] flex justify-center relative">
+      <div className="flex justify-center gap-[107px]">
+        <div className="relative self-start mt-3">
+          <input
+            type="text"
+            placeholder="Digite o que deseja buscar"
+            className="rounded-full py-3 px-4 border border-[#CACACA] text-xs w-md placeholder-[#404042] bg-white">
+          </input>
+          <div className="absolute h-full right-[15px] top-0 flex items-center"><img src="/images/UI/magnifying-glass.svg" alt="Ícone de busca" /></div>
+        </div>
+
+        <div>
+          <h1 className="text-4xl text-[#404042] font-normal leading-10 tracking-[-0.72px]">
+            <span className="text-[#BE202E] font-semibold">Tudo em um só lugar:</span><br />
+            Consultas, Exames,<br />
+            Cirurgias e muito mais!
+          </h1>
+
+          <button 
+            className="color-[#404042] rounded-full border-[1.111px] border-[rgba(64, 64, 66, 0.70)] py-[17px] px-[18.06px] mt-8 cursor-pointer tracking-[-0.32px] flex gap-[5.55px] items-center leading-0">
+            Marque com especialista
+            <img src="/images/UI/arrowright.svg" alt="Ícone de seta para direita" />
+          </button>
+        </div>
+      </div>
+
+      <Image
+        src="/images/background-hero.png"
+        alt="Imagem de fundo" 
+        width={1280}
+        height={284}
+        className="absolute top-0 left-0 -z-10 w-full h-full object-fill"/>
+    </section>
+  );
+}
