cd48c26470948d989509dde36a115d4dd36c1eec
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-10-31 11:45:39 -0300

    style: PaymentFeatures spacing adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <Specialties specialties={["Cardiologia", "Endocrinologia", "Obstetrícia", "Gastroenterologia", "Cirurgia Geral", "Neurologia", "Ortopedia", "Dermatologia"]}/>
       <PaymentFeatures features={[
         {icon: "/images/UI/pix-icon.svg", title: "5% OFF NO PIX", description: "Desconto no pagamento antecipado"},
-        {icon: "/images/UI/card-icon.svg", title: "10x sem juros", description: "Para compras a partir de R$500,00"},
+        {icon: "/images/UI/card-icon.svg", title: "10X sem juros", description: "Para compras a partir de R$500,00"},
         {icon: "/images/UI/percent-icon.svg", title: "DESCONTOS ESPECIAIS", description: "Planos e pacotes promocionais"},
-        {icon: "/images/UI/fast-clock-icon.svg", title: "10x sem juros", description: "Para compras a partir de R$500,00"},
+        {icon: "/images/UI/fast-clock-icon.svg", title: "ATENDIMENTO PERSONALIZADO", description: "Agendamento sem burocracia"},
       ]}/>
     </>
   );
