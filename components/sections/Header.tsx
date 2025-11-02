f9cf0512be9bb089fc491dff9c2087a65d8f7ce0
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-11-02 19:45:58 -0300

    style: header now is hided when scrolling down

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,8 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import HeaderCarousel from "../HeaderCarousel";
 import HeaderContent from "@/components/HeaderContent";
 import HeaderNav from "@/components/HeaderNav";
 
 export default function Header() {
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowHeader(false); // rolando para baixo
+      } else {
+        setShowHeader(true); // rolando para cima
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   const HeaderCarouselProps = {
     slides: ["AGENDE 3 PAGUE 2: confira as especialidades", "slide 2", "slide 3"],
     options: { loop: true }
@@ -10,9 +34,13 @@ export default function Header() {
 
   return (
     <header>
-      <div className="h-[159.72px]"/>
-      <div className="fixed top-0 bg-white w-full z-20">
-        <HeaderCarousel {...HeaderCarouselProps}/>
+      <div className="h-[159.72px]" />
+      <div
+        className={`fixed top-0 bg-white w-full z-20 transition-transform duration-300 ${
+          showHeader ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <HeaderCarousel {...HeaderCarouselProps} />
         <HeaderContent />
         <HeaderNav />
       </div>
