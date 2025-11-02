30caa7cd98beb61e05c4e7c62e923c2a6d18d522
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-11-02 19:00:41 -0300

    style: hover effect in images from Packs section

--- components/sections/Packs.tsx
+++ components/sections/Packs.tsx
@@ -27,7 +27,7 @@ export default function Packs() {
     <p className="leading-[130%] tracking-[-0.4px] text-xl text-center">Aproveite as melhores condições pensadas para você</p>
     <div className="flex gap-4">
       {packs.map((pack, index) => (<div key={index} className="bg-[#F1F1F1] rounded-2xl flex flex-col items-center gap-[18px] py-6 px-4 w-[268px]">
-        <Image src={pack.image} alt={pack.imageAlt || "pack image"} width={222} height={177} />
+        <div className="overflow-hidden rounded-2xl"><Image className="hover:scale-110 transition-all" src={pack.image} alt={pack.imageAlt || "pack image"} width={222} height={177} /></div>
         <h3 className="text-[#056939] text-center text-lg font-semibold tracking-[-0.36px] leading-none">{pack.title}</h3>
         <p className="text-center text-sm leading-[161.394%] tracking-[-0.28px]">{pack.description}</p>
         <a href="/" className="cursor-pointer flex gap-2">Saber mais <img src="/images/UI/arrowright.svg" width={6} height={10} alt="seta para a direita" /></a>
